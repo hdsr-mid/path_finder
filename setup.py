6d950d1c532d37c9ef721fd028b433e871a71dd5
renierkramerhdsr <renier.kramer@hdsr.nl>
2021-01-07 19:48:00 +0100

    stable release

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 
-version = "0.1"
+version = "0.2"
 
-long_description = "\n\n".join([open("README.rst").read()])
+long_description = "\n\n".join([open("README.md").read()])
 
 install_requires = [
     "pathlib",
@@ -24,18 +24,17 @@ setup(
     author="Renier Kramer",
     author_email="renier.kramer@hdsr.nl",
     url="https://github.com/hdsr-mid/path_finder",
-    download_url="xx",
+    download_url="https://github.com/hdsr-mid/path_finder/archive/v0.2.tar.gz",
     keywords=["interface", "path", "directory", "filename", "glob", "regex", "find"],
     install_requires=install_requires,
     tests_require=tests_require,
     extras_require={"test": tests_require},
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
